4c4dcd6229a6d1cfb8e188f32d53603026238d15
Mack Peters <mpeters@harding.edu>
2017-10-30 09:51:21 -0500

    Just getting started. Created functionality to get the file to be decompressed, get the file contents as well as file length, and started working on getting the file name length. Need to figure out if we have to determine whether it is using 2 or 4 bytes for ints or if we can just assume one or the other. Test files indicate 4, but just to be sure it's worth asking.

--- puff/puff.cpp
+++ puff/puff.cpp
@@ -1,11 +1,67 @@
 // puff.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+
+using std::map;
+using std::vector;
+using std::string;
+
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 
-int main()
+struct pufFile {
+	int realNameLength;
+	string realName;
+	string pufName;
+	int numberOfGlyphs;
+	ifstream compressedFile;
+	double compressedFileLength;
+};
+
+// TODO close file
+// TODO map file into memory (https://stackoverflow.com/questions/15138353/how-to-read-a-binary-file-into-a-vector-of-unsigned-chars)
+void loadFileContents(pufFile &pufFile)
 {
-    return 0;
+	pufFile.compressedFile = ifstream(pufFile.pufName, ios::in | ios::binary);
+
+	pufFile.compressedFile.seekg(0, pufFile.compressedFile.end);
+	pufFile.compressedFileLength = pufFile.compressedFile.tellg();
+	pufFile.compressedFile.seekg(0, pufFile.compressedFile.beg);
+
 }
 
+// TODO Figure out how to determine 2 or 4 byte integers. Assuming 4 for now based on coach's test .huf file
+void getFileNameLengthAndName(pufFile &pufFile)
+{
+
+}
+
+void decompressFile(string fileToDecompress)
+{
+	pufFile pufFile;
+	pufFile.pufName = fileToDecompress;
+
+	loadFileContents(pufFile);
+	getFileNameLengthAndName(pufFile);
+}
+
+void main()
+{
+	string fileToDecompress;
+	cout << "Enter the name of the .huf file to be decompressed: ";
+	getline(cin, fileToDecompress);
+	decompressFile(fileToDecompress);
+}
