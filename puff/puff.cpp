ca88d0bed8319a1fc148d11bc563dd37ab8ee208
Mack Peters <mpeters@harding.edu>
2017-11-01 17:01:46 -0500

    Added Coach's example file. Also got functionality working to get name length and the name, then added functions to get the length of the huffman table and the huffman table itself. Included a struct definition for huffTableValues. Also added a function that returns if a bit is turned on to determine byte values.

--- puff/puff.cpp
+++ puff/puff.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <string>
 #include <iomanip>
+#include <cmath>
 
 using std::map;
 using std::vector;
@@ -22,11 +23,18 @@ using std::cout;
 using std::endl;
 
 
+struct huffTableEntry {
+	int glyph = -1;
+	int leftPointer = -1;
+	int rightPointer = -1;
+};
+
 struct pufFile {
-	int realNameLength;
-	string realName;
+	int realNameLength = 0;
+	string realName = "";
 	string pufName;
-	int numberOfGlyphs;
+	int numberOfGlyphs = 0;
+	huffTableEntry hufTable[513];
 	ifstream compressedFile;
 	double compressedFileLength;
 };
@@ -43,9 +51,92 @@ void loadFileContents(pufFile &pufFile)
 
 }
 
+bool determineBitOrientation(unsigned char byte, int bitPosition)
+{
+	if (byte & (short)pow(2.0, bitPosition))
+		return true;
+	else
+		return false;
+}
+
 // TODO Figure out how to determine 2 or 4 byte integers. Assuming 4 for now based on coach's test .huf file
 void getFileNameLengthAndName(pufFile &pufFile)
 {
+	unsigned char c[1];
+	int bitTracker = 0;
+	//loop four times to get 4 bytes of name length (based on however long we decide our integers will be. Coach's are 4 bytes)
+	for (int i = 0; i < 4; i++)
+	{
+		pufFile.compressedFile.read((char*)c, 1);
+		pufFile.compressedFile.seekg(0, 1);
+
+		for (int j = 0; j < 8; j++)
+		{
+			pufFile.realNameLength += determineBitOrientation(c[0], j) ? (short)pow(2.0, bitTracker) : 0;
+			bitTracker++;
+		}
+	}
+
+	//loop until we've gotten the whole length of the file
+	for (int i = 0; i < pufFile.realNameLength; i++)
+	{
+		pufFile.compressedFile.read((char*)c, 1);
+		pufFile.compressedFile.seekg(0, 1);
+
+		pufFile.realName += c[0];
+	}
+}
+
+void getHuffmanTable(pufFile &pufFile)
+{
+	unsigned char c[1];
+	int bitTracker = 0;
+	int bitValue = 0;
+
+	//loops through 4 bytes to get huffman length
+	for (int i = 0; i < 4; i++)
+	{
+		pufFile.compressedFile.read((char*)c, 1);
+		pufFile.compressedFile.seekg(0, 1);
+
+		for (int j = 0; j < 8; j++)
+		{
+			pufFile.numberOfGlyphs += determineBitOrientation(c[0], j) ? (short)pow(2.0, bitTracker) : 0;
+			bitTracker++;
+		}
+	}
+
+	//this is getting crazy and can be done better, but first loop is for the
+	//slot in huffman table array, second loop is for glyph vs left pointer vs right pointer,
+	//third loop is for the values gotten from file, fourth is for bit orientation.
+	for (int i = 0; i < pufFile.numberOfGlyphs; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			bitTracker = 0;
+			bitValue = 0;
+			for (int k = 0; k < 4; k++)
+			{
+				pufFile.compressedFile.read((char*)c, 1);
+				pufFile.compressedFile.seekg(0, 1);
+
+				for (int l = 0; l < 8; l++)
+				{
+					bitValue += determineBitOrientation(c[0], l) ? (short)pow(2.0, bitTracker) : 0;
+					bitTracker++;
+				}
+			}
+			if (bitValue > 256)
+				bitValue = -1;
+			if (j == 0)
+				pufFile.hufTable[i].glyph = bitValue;
+			else if (j == 1)
+				pufFile.hufTable[i].leftPointer = bitValue;
+			else
+				pufFile.hufTable[i].rightPointer = bitValue;
+		}
+		cout << (char)pufFile.hufTable[i].glyph << ' ' << pufFile.hufTable[i].leftPointer << ' ' << pufFile.hufTable[i].rightPointer << endl;
+	}
 
 }
 
@@ -56,6 +147,7 @@ void decompressFile(string fileToDecompress)
 
 	loadFileContents(pufFile);
 	getFileNameLengthAndName(pufFile);
+	getHuffmanTable(pufFile);
 }
 
 void main()
