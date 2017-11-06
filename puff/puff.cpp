19cf52b8f1b0d495fd57ccce3bebb0cf8eaa8f8d
Mack Peters <mpeters@harding.edu>
2017-11-06 09:54:34 -0600

    Commenting
    And then also getting rid of some things that evidently aren't necessary. Got it down to 8.2 from 8.3 for time!

--- puff/puff.cpp
+++ puff/puff.cpp
@@ -1,5 +1,7 @@
-// puff.cpp : Defines the entry point for the console application.
-//
+// puff.cpp
+//By Jerred Shepherd and Mack Peters
+//This program takes a file that has been previously compressed by its corresponding
+//huff version and decompresses it to produce a lossless version of what has been compressed.
 
 #include <iostream>
 #include <fstream>
@@ -23,13 +25,16 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-
+//Defines how an entry should look in the huffman table
 struct huffTableEntry {
     int glyph = -1;
     int leftPointer = -1;
     int rightPointer = -1;
 };
 
+//Contains the contents of the hufFile, as well as some pieces of information that pertain
+//to its decoding. hufTable is generated based on the contents of the file and is what allows
+//the file to be decoded. byteCodes is generated once hufTable is completed.
 struct pufFile {
     int realNameLength = 0;
     string realName = "";
@@ -41,74 +46,75 @@ struct pufFile {
     map<string, int> byteCodes;
 };
 
-// TODO close file
-// TODO map file into memory (https://stackoverflow.com/questions/15138353/how-to-read-a-binary-file-into-a-vector-of-unsigned-chars)
 void loadFileContents(pufFile &pufFile) {
     pufFile.compressedFile = ifstream(pufFile.pufName, ios::in | ios::binary);
 }
 
+//Determines whether a bit is on or off and returns the result
 bool determineBitOrientation(unsigned char byte, int bitPosition) {
     return (byte & (short) pow(2.0, bitPosition)) != 0;
 }
 
-// TODO Figure out how to determine 2 or 4 byte integers. Assuming 4 for now based on coach's test .huf file
+//Determines how long the file name is and then gets the file name
 void getFileNameLengthAndName(pufFile &pufFile) {
-    unsigned char c[1];
+    unsigned char c;
     int bitTracker = 0;
-    //loop four times to get 4 bytes of name length (based on however long we decide our integers will be. Coach's are 4 bytes)
+
+    //loop four times to get 4 bytes of name length
     for (int i = 0; i < 4; i++) {
-        pufFile.compressedFile.read((char *) c, 1);
+        pufFile.compressedFile.read((char *)&c, 1);
         pufFile.compressedFile.seekg(0, 1);
 
         for (int j = 0; j < 8; j++) {
-            pufFile.realNameLength += determineBitOrientation(c[0], j) ? (short) pow(2.0, bitTracker) : 0;
+            pufFile.realNameLength += determineBitOrientation(c, j) ? (short) pow(2.0, bitTracker) : 0;
             bitTracker++;
         }
     }
 
     //loop until we've gotten the whole length of the file
     for (int i = 0; i < pufFile.realNameLength; i++) {
-        pufFile.compressedFile.read((char *) c, 1);
+        pufFile.compressedFile.read((char *)&c, 1);
         pufFile.compressedFile.seekg(0, 1);
 
-        pufFile.realName += c[0];
+        pufFile.realName += c;
     }
 }
 
+//Generates the huffman table from the contents of file. First gets the length
+//of the huffman table, then loops to get all the values and fill the table.
 void getHuffmanTable(pufFile &pufFile) {
-    unsigned char c[1];
+    unsigned char c;
     int bitTracker = 0;
     int bitValue = 0;
 
     //loops through 4 bytes to get huffman length
     for (int i = 0; i < 4; i++) {
-        pufFile.compressedFile.read((char *) c, 1);
+        pufFile.compressedFile.read((char *)&c, 1);
         pufFile.compressedFile.seekg(0, 1);
 
         for (int j = 0; j < 8; j++) {
-            pufFile.numberOfGlyphs += determineBitOrientation(c[0], j) ? (short) pow(2.0, bitTracker) : 0;
+            pufFile.numberOfGlyphs += determineBitOrientation(c, j) ? (short) pow(2.0, bitTracker) : 0;
             bitTracker++;
         }
     }
 
-    //this is getting crazy and can be done better, but first loop is for the
-    //slot in huffman table array, second loop is for glyph vs left pointer vs right pointer,
+    // first loop is for the slot in huffman table array, second loop is for glyph(first) vs left pointer(second) vs right pointer(third),
     //third loop is for the values gotten from file, fourth is for bit orientation.
     for (int i = 0; i < pufFile.numberOfGlyphs; i++) {
         for (int j = 0; j < 3; j++) {
             bitTracker = 0;
             bitValue = 0;
             for (int k = 0; k < 4; k++) {
-                pufFile.compressedFile.read((char *) c, 1);
+                pufFile.compressedFile.read((char *)&c, 1);
                 pufFile.compressedFile.seekg(0, 1);
 
                 for (int l = 0; l < 8; l++) {
-                    bitValue += determineBitOrientation(c[0], l) ? (short) pow(2.0, bitTracker) : 0;
+                    bitValue += determineBitOrientation(c, l) ? (short) pow(2.0, bitTracker) : 0;
                     bitTracker++;
                 }
             }
-            if (bitValue == 65535)
-                bitValue = -1;
+           /* if (bitValue == 65535)
+                bitValue = -1;*/
             if (j == 0)
                 pufFile.hufTable[i].glyph = bitValue;
             else if (j == 1)
@@ -116,12 +122,14 @@ void getHuffmanTable(pufFile &pufFile) {
             else
                 pufFile.hufTable[i].rightPointer = bitValue;
         }
-        //Debug statement
-        /*cout << pufFile.hufTable[i].glyph << ' ' << pufFile.hufTable[i].leftPointer << ' ' << pufFile.hufTable[i].rightPointer << endl;*/
     }
 
 }
 
+//Recursive function that generates byte codes. If the table entry is a merge node (-1 as glyph), we then look for
+//left and right pointers. If the pointer goes left, add a 0 to the byte code, if it goes right, add a 1. Once we hit
+//a leaf node (glyph not -1) then attach the string to that glyph. Stored in a map of <string, int> where the string is the
+//bytecode and the int is the glyph. Ints are necessary because 256 is EOF and cannot be represented as a char.
 void generateByteCodes(huffTableEntry hufTable[], map<string, int> &byteCodes, int position, string byteCode) {
     if (hufTable[position].leftPointer != -1 || hufTable[position].rightPointer != -1) {
         if (hufTable[position].leftPointer != -1) {
@@ -135,18 +143,14 @@ void generateByteCodes(huffTableEntry hufTable[], map<string, int> &byteCodes, i
     }
 }
 
+//Acts as an entry point to generate the byte codes. Implemented in order to keep other functions more organized
 void generateByteCodeTable(pufFile &pufFile) {
     string byteCode;
 
     generateByteCodes(pufFile.hufTable, pufFile.byteCodes, 0, byteCode);
-
-    //Debug Statement
-    /*for (auto elem : pufFile.byteCodes) {
-        std::cout << elem.first << " " << std::setfill('0') << std::setw(2) << std::uppercase << elem.second
-            << "\n";
-    }*/
 }
 
+//Gets the contents of file as a long string of bits to be read later and decoded
 string getEncodedMessage(pufFile &pufFile) {
     //Get the current position in the file, get the file length, then go back to the place that was left from
     int currentPosition = pufFile.compressedFile.tellg();
@@ -155,19 +159,15 @@ string getEncodedMessage(pufFile &pufFile) {
     pufFile.compressedFile.seekg(currentPosition);
 
     string s = "";
-    int bitTracker = 0;
 
     for (int i = currentPosition; i < pufFile.compressedFileLength; i++) {
-        unsigned char c[1];
-        pufFile.compressedFile.read((char *) c, 1);
+        unsigned char c;
+        pufFile.compressedFile.read((char *)&c, 1);
         pufFile.compressedFile.seekg(0, 1);
 
         for (int j = 0; j < 8; j++) {
-            s += determineBitOrientation(c[0], j) ? "1" : "0";
-            bitTracker++;
+            s += determineBitOrientation(c, j) ? "1" : "0";
         }
-
-        bitTracker = 0;
     }
     return s;
 }
